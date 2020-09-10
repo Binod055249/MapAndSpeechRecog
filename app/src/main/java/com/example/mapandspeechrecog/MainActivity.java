3a50ebd26213e6220c818c8ef5fbe676b4ab2d48
Binod Kumar <vk055249@gmail.com>
2020-09-10 19:10:59 +0530

    Speech Recognition and display the word said By user

---
+++ app/src/main/java/com/example/mapandspeechrecog/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.mapandspeechrecog;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int SPEAK_REQUEST = 10;
+
+    TextView txtValue;
+    Button btnVoiceIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtValue = findViewById(R.id.txtValue);
+        btnVoiceIntent = findViewById(R.id.btnVoiceIntent);
+
+        btnVoiceIntent.setOnClickListener(this);
+
+        PackageManager packageManager = this.getPackageManager();
+        List<ResolveInfo> listOfInformation = packageManager.queryIntentActivities(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),0);
+
+        if (listOfInformation.size()>0) {
+
+            Toast.makeText(this,"Your Device Does Support Speech Recognition!",Toast.LENGTH_SHORT).show();
+             listenToTheUserVoice();
+        }else {
+
+            Toast.makeText(this,"Your Device Does Not Support Speech Recognition!",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        listenToTheUserVoice();
+    }
+
+    private void listenToTheUserVoice(){
+
+        Intent voiceIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk To Me!");
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+        startActivityForResult(voiceIntent, SPEAK_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SPEAK_REQUEST && resultCode == RESULT_OK){
+
+            ArrayList<String> voiceWords = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            float[] confidLevels = data.getFloatArrayExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
+            int index = 0;
+            for (String userWord : voiceWords) {
+
+                if (confidLevels != null && index < confidLevels.length) {
+
+                    txtValue.setText(userWord + " - " + confidLevels[index]);
+                }
+
+            }
+        }
+    }
+}
